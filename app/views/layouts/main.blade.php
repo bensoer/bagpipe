3ef249274146c23ec38ffd0c0a9f9dd4f78aa474
Ryan Sadio <ryan@sadio.ca>
2014-11-18 14:42:50 -0800

    favicon

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -9,6 +9,23 @@
 
     <title>Bagpipe - @yield('meta-title')</title>
 
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('/apple-touch-icon-57x57.png') }}" >
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('/apple-touch-icon-114x114.png') }}" >
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('/apple-touch-icon-72x72.png') }}" >
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('/apple-touch-icon-144x144.png') }}" >
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('/apple-touch-icon-60x60.png') }}" >
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('/apple-touch-icon-120x120.png') }}" >
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('/apple-touch-icon-76x76.png') }}" >
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('/apple-touch-icon-152x152.png') }}" >
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon-180x180.png') }}" >
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-192x192.png" sizes="192x192') }}" >
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-160x160.png" sizes="160x160') }}" >
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-96x96.png" sizes="96x96') }}" >
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-16x16.png" sizes="16x16') }}" >
+    <link rel="icon" type="image/png" href="{{ asset('/favicon-32x32.png" sizes="32x32') }}" >
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+
     <!-- Bootstrap Core CSS -->
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
