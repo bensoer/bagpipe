afb472402452d804d9ab25b55306ee8dbb357941
Ryan Sadio <ryan@sadio.ca>
2014-11-18 23:57:24 -0800

    about page

--- app/views/pages/about.blade.php
+++ app/views/pages/about.blade.php
@@ -6,15 +6,20 @@
 @section('content')
 
     <!-- Header -->
-    <header id="top" class="header">
+    <header id="top" class="header_about">
         <div class="text-vertical-center">
-            <div class="col-xs-4 col-lg-offset-4">
-                <h1>Bagpipe</h1>
-                <h3>We are all Bagpipers! Music is part of everyones life and day, and Youtube has become the main influence
+            <div class="col-xs-4 col-lg-offset-4 about">
+                <h1 class="text-left">Bagpipe</h1>
+                <br />
+                <p class="text-left lead">We are all Bagpipers!</p>
+                <p class="text-left">Music is part of everyones life and day, and Youtube has become the main influence
                 of our exploration of music. Bagpipe choses to harness that influence by creating an interface for you to
-                create your playlists for parties or social events. No need to hire a DJ, just create a playlist on Bagpipe.
+                create your playlists for parties or social events.</p>
+                <p class="text-left">No need to hire a DJ, just create a playlist on Bagpipe.
                 If your guests would like to contribute to the playlist, simply share your playlist pin and others can submit
-                songs to your playlist. The more votes a song gets, the higher up the playlist it moves!</h3>
+                songs to your playlist. The more votes a song gets, the higher up the playlist it moves!</p>
+                <br />
+                <div class="text-right"><a href="/">Ekk! Where's the back button?</a></div>
             </div>
         </div>
     </header>
