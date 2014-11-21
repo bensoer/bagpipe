a97458267633c25fa40d1dba5c965d886c98d3a1
Ryan Sadio <ryan@sadio.ca>
2014-11-21 08:52:19 -0800

    Mobile view fixes on the branding and menu bar

--- app/views/layouts/party.blade.php
+++ app/views/layouts/party.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- Header Bar -->
-<nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-default navbar-custom navbar-fixed-top" role="navigation">
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header page-scroll">
