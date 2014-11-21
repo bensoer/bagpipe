a1e46fd8a59377fc355504c738b60597b0a2e7dc
bensoer <ikben@shaw.ca>
2014-11-21 13:46:06 -0800

    added notes for project movements

--- app/views/party/guest.blade.php
+++ app/views/party/guest.blade.php
@@ -27,8 +27,7 @@
                                 <h1>Now Playing</h1>
                                 <!-- JavaScript loaded now playing list -->
                                 <p id="label"></p></p>
-                                <!-- Youtube player module -->
-                                <div id="player"></div>
+
 
                          </div>
                      </div>
@@ -39,16 +38,6 @@
                 <div class="col-lg-12 text-center">
                     <div class="col-lg-12 center-block">
                          <div class="form-group" style="border: 2px solid black;text-align:center">
-                              <div >
-                                    <button type="button" class="btn btn-default" style="float:left" onclick="goToPrevious()">Play Previous</button>
-                                    <button type="button" class="btn btn-default" style="float:left" onclick="stopVideo()">Stop Playlist</button>
-
-
-                                    <h1 style="display:inline">Up Next</h1>
-
-                                    <button type="button" class="btn btn-default" style="float:right" onclick="goToNext()">Play Next</button>
-                                    <button type="button" class="btn btn-default" style="float:right" onclick="playVideo()">Play/Resume Playlist</button>
-                              </div>
 
                                 <!-- JavaScript loaded up next list-->
                               <div id="next"><ul id="list" class="list-group" style="list-style-type:none"></ul></div>
