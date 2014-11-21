5a13a4c1061d78bd5d94eadec301cffc40660afd
Ryan Sadio <ryan@sadio.ca>
2014-11-21 14:12:23 -0800

    Search and controls ui

--- app/views/party/guest.blade.php
+++ app/views/party/guest.blade.php
@@ -19,44 +19,60 @@
 
 @section('control')
 
-<div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="col-lg-12 center-block">
-                         <div class="form-group prev_next">
-
-                                <h1>Now Playing</h1>
-                                <!-- JavaScript loaded now playing list -->
-                                <p id="label"></p></p>
-                                <!-- Youtube player module -->
-                                <div id="player"></div>
-
-                         </div>
-                     </div>
-                 </div>
-            </div>
-
+<!-- Currently playing -->
+<div id="current-song">
     <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="col-lg-12 center-block">
-                         <div class="form-group" style="border: 2px solid black;text-align:center">
-                              <div >
-                                    <button type="button" class="btn btn-default" style="float:left" onclick="goToPrevious()">Play Previous</button>
-                                    <button type="button" class="btn btn-default" style="float:left" onclick="stopVideo()">Stop Playlist</button>
+        <div class="col-lg-12 text-center">
+            <div class="col-lg-12 center-block">
+                 <div class="form-group prev_next">
 
+                        <!-- JavaScript loaded now playing list -->
+                        <p id="label"></p>
+                        <!-- Youtube player module -->
+                        <div id="player"></div>
 
-                                    <h1 style="display:inline">Up Next</h1>
+                 </div>
 
-                                    <button type="button" class="btn btn-default" style="float:right" onclick="goToNext()">Play Next</button>
-                                    <button type="button" class="btn btn-default" style="float:right" onclick="playVideo()">Play/Resume Playlist</button>
-                              </div>
+                     <button type="button" class="btn btn-default" style="float:left" onclick="goToPrevious()">Play Previous</button>
+                     <button type="button" class="btn btn-default" style="float:left" onclick="stopVideo()">Stop Playlist</button>
+                     <button type="button" class="btn btn-default" style="float:right" onclick="goToNext()">Play Next</button>
+                     <button type="button" class="glyphicon glyphicon-play" aria-hidden="true" style="float:right" onclick="playVideo()">Play/Resume Playlist</button>
+                     <span class="glyphicon glyphicon-play-circle" aria-hidden="true" onclick="playVideo()"></span>
 
-                                <!-- JavaScript loaded up next list-->
-                              <div id="next"><ul id="list" class="list-group" style="list-style-type:none"></ul></div>
-
-                         </div>
-                    </div>
-                </div>
+             </div>
+         </div>
     </div>
+</div>
+
+<!-- Queue of YT videos -->
+<div id="queue-list">
+    <div class="row">
+        <div class="col-lg-12 text-center">
+                 <div class="form-group" style="border: 2px solid black;text-align:center">
+
+                            <ul>
+                                <li class="queue-item">
+                                    Video 1
+                                </li >
+
+                                <li class="queue-item">
+                                    Video 2
+                                </li>
+                                <li class="queue-item">
+                                    Video 3
+                                </li>
+
+                            </ul>
+
+
+
+                        <!-- JavaScript loaded up next list-->
+                      <div id="next"><ul id="list" class="list-group" style="list-style-type:none"></ul></div>
+
+                 </div>
+        </div>
+    </div>
+</div>
 
 
 @stop
