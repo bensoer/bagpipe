60a2054d230f4db4060d2605348ec701e098c13e
bensoer <ben@soernet.ca>
2015-10-03 23:19:44 -0700

    added server as production

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'production' => array('zealous-briars'),
+    'production' => array('zealous-briars', 'ratchet'),
     'local' => array('APACHESERV', 'homestead', '.local', 'ironhide'),
 
 ));
